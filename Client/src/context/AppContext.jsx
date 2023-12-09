cb7f49a1892907bec0b2c74dbfd21240f64b1684
VMarkMellor <tranvinh2503qn@gmail.com>
2023-12-09 15:37:09 +0700

    merge client

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -11,19 +11,19 @@ export const AppProvider = ({ children }) => {
   const [courses, SetCourses] = useState([]);
   const [myCourses, SetMyCourses] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/api/all/courses_from_server", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        SetCourses(data);
-      })
-      .catch(console.error());
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/api/all/courses_from_server", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       SetCourses(data);
+  //     })
+  //     .catch(console.error());
+  // }, []);
 
   function getUserFromToken(token) {
     try {
@@ -49,19 +49,24 @@ export const AppProvider = ({ children }) => {
   }, [id]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/my/courses", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    })
+    if (user && Object.keys(user).length > 0) {
+      fetch("http://localhost:8000/api/my/courses", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
       .then((response) => response.json())
       .then((data) => {
         SetMyCourses(data);
       })
-      .catch(console.error());
+      .catch((error) => {
+        console.error(error);
+      });
+    }
   }, [user]);
+  
 
   return (
     <AppContext.Provider value={{ user, courses, myCourses }}>
